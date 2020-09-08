f12b5af62a1c67ae00f6442708058ce1bbed1802
Diego Clair <diego93rodrigues@gmail.com>
2020-09-08 00:51:08 -0300

    Created logic to write a csv file from data returned by topic

--- main.go
+++ main.go
@@ -1,53 +1,174 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
-	"log"
+	"reflect"
+	"strconv"
+
+	//import the Paho Go MQTT library
 	"os"
+	"strings"
 	"time"
 
-	mqtt "github.com/eclipse/paho.mqtt.golang"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
-var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("TOPIC: %s\n", msg.Topic())
-	fmt.Printf("MSG: %s\n", msg.Payload())
+var flag bool = false
+var i int
+
+// Rows csv data contract
+type Rows interface {
+	ToCSVHeader() []string
+	ToCSVRow() []string
+}
+
+type Data struct {
+	Tempo        string `json:"Tempo" csv:"Tempo" `
+	Estacao      string `json:"Estação" csv:"Estação" `
+	LAT          string `json:"LAT" csv:"LAT" `
+	LONG         string `json:"LONG" csv:"LONG" `
+	Movimentacao string `json:"Movimentação" csv:"Movimentação" `
+	Original473  string `json:"Original_473" csv:"Original_473" `
+	Original269  string `json:"Original_269" csv:"Original_269" `
+	Zero         string `json:"Zero" csv:"Zero" `
+	MacaVerde    string `json:"Maçã-Verde" csv:"Maçã-Verde" `
+	Tangerina    string `json:"Tangerina" csv:"Tangerina" `
+	Citrus       string `json:"Citrus" csv:"Citrus" `
+	AcaiGuarana  string `json:"Açaí-Guaraná" csv:"Açaí-Guaraná" `
+	Pessego      string `json:"Pêssego" csv:"Pêssego" `
+	TARGET       string `json:"TARGET" csv:"TARGET" `
+	Row          int    `json:"row" csv:"row" `
+}
+
+// ToCSVRow Return struct in a csv row format
+func (d Data) ToCSVRow() []string {
+	return []string{
+		strconv.Itoa(d.Row),
+		d.Tempo,
+		d.Estacao,
+		d.LAT,
+		d.LONG,
+		d.Movimentacao,
+		d.Original473,
+		d.Original269,
+		d.Zero,
+		d.MacaVerde,
+		d.Tangerina,
+		d.Citrus,
+		d.AcaiGuarana,
+		d.Pessego,
+		d.TARGET,
+	}
+}
+
+//ToCSVHeader returns the headers of csv based on tag "csv" on struct fields
+func (d Data) ToCSVHeader() []string {
+	t := reflect.TypeOf(d)
+
+	retVal := make([]string, t.NumField())
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("csv")
+		retVal[i] = tag
+	}
+	return retVal
+}
+
+//define a function for the default message handler
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	payload := msg.Payload()
+	if strings.Compare(string(payload), "\n") > 0 {
+
+		var data Data
+
+		json.Unmarshal([]byte(payload), &data)
+
+		err := writeCsvFile(data)
+		if err != nil {
+			flag = true
+		}
+		fmt.Printf("MSG: %s\n", payload)
+
+		if i == 3 {
+			flag = true
+		}
+	}
+
+	if strings.Compare("bye\n", string(payload)) == 0 {
+		fmt.Println("exitting")
+		flag = true
+	}
 }
 
 func main() {
-	mqtt.DEBUG = log.New(os.Stdout, "", 0)
-	mqtt.ERROR = log.New(os.Stdout, "", 0)
-	opts := mqtt.NewClientOptions().AddBroker("tcp://broker.emqx.io:1883").SetClientID("emqx_test_client")
-
-	opts.SetKeepAlive(60 * time.Second)
-	// Set the message callback handler
+	//create a ClientOptions struct setting the broker address, clientid, turn
+	//off trace output and set the default message handler
+	opts := MQTT.NewClientOptions().AddBroker("tcp://tnt-iot.maratona.dev:30573")
+	opts.SetClientID("Device-sub")
 	opts.SetDefaultPublishHandler(f)
-	opts.SetPingTimeout(1 * time.Second)
+	opts.SetUsername("maratoners")
+	opts.SetPassword("ndsjknvkdnvjsbvj")
 
-	c := mqtt.NewClient(opts)
+	//create and start a client using the above ClientOptions
+	c := MQTT.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
-	// Subscribe to a topic
-	if token := c.Subscribe("testtopic/#", 0, nil); token.Wait() && token.Error() != nil {
+	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
+	//at a maximum qos of zero, wait for the receipt to confirm the subscription
+	if token := c.Subscribe("tnt", 0, nil); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
 
-	// Publish a message
-	token := c.Publish("testtopic/1", 0, false, "Hello World")
-	token.Wait()
+	for flag == false {
+		time.Sleep(1 * time.Second)
+	}
 
-	time.Sleep(6 * time.Second)
-
-	// Unscribe
-	if token := c.Unsubscribe("testtopic/#"); token.Wait() && token.Error() != nil {
+	//unsubscribe from /go-mqtt/sample
+	if token := c.Unsubscribe("tnt"); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
 
-	// Disconnect
 	c.Disconnect(250)
-	time.Sleep(1 * time.Second)
+}
+
+func writeCsvFile(data Data) error {
+
+	path := "./dataset.csv"
+	fileCSV, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY|os.O_EXCL, 0666)
+	if err != nil {
+		fmt.Println("Error to create file: ", err)
+		return err
+	}
+
+	w := bufio.NewWriterSize(fileCSV, 4096*2)
+	wr := csv.NewWriter(w)
+
+	fileInfo, err := fileCSV.Stat()
+	if err != nil {
+		return err
+	}
+
+	if i == 0 && fileInfo.Size() == 0 {
+		wr.Write(data.ToCSVHeader())
+	}
+	i++
+	wr.Write(data.ToCSVRow())
+
+	wr.Flush()
+
+	_, err = fileCSV.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+
+	fileCSV.Close()
+
+	return nil
 }
